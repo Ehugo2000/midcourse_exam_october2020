fac79f8cd884d76c47a78c2ca175809d556f0636
Ehugo2000 <erik.hgsn@gmail.com>
2020-12-03 11:08:53 +0100

    Installing yarn and clean scaffold code

--- src/components/GHSearch.jsx
+++ src/components/GHSearch.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import { Button, Input } from 'semantic-ui-react'
+import React from "react";
+import { Button, Input } from "semantic-ui-react";
 
 const GHSearch = () => {
   return (
     <>
-      <Input type="text" name="search" placeholder="Input GH username"/>
+      <Input type="text" name="search" placeholder="Input GH username" />
       <Button name="search">Search</Button>
     </>
-  )
-}
+  );
+};
 
-export default GHSearch
+export default GHSearch;
